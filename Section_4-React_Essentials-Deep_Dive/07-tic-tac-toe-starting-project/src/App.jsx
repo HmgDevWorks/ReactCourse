877553a9182a0220f234bcb0be3ba23bc1d60dcb
Hmg <HmgDevWorks@gmail.com>
2025-02-17 07:09:23 +0100

    Removed currentPlayer state, disabled game buttons depends on its value

--- Section_4-React_Essentials-Deep_Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section_4-React_Essentials-Deep_Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,22 +4,31 @@ import Player from "./componentts/Player"
 import GameBoard from "./componentts/GameBoard"
 import Log from "./componentts/log"
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayerSymbol = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayerSymbol = 'O';
+  }
+
+  return currentPlayerSymbol;
+}
+
 function App() {
   const [gameTurns, setGameTurn] = useState([]);
-  const [currentPlayer, setCurrentPlayer] = useState('X');
+
+  const currentPlayerSymbol = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setCurrentPlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
     setGameTurn((prevTurns) => {
-      let currentPlayerSymbol = currentPlayer === 'X' ? 'X' : 'O';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayerSymbol = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
       const updatedTurns = [
         {
           square: { row: rowIndex, col: colIndex },
-          player: currentPlayerSymbol
+          player: currentPlayer
         }, ...prevTurns];
+
       return updatedTurns;
     });
   }
@@ -28,8 +37,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'} />
+          <Player initialName="Player 1" symbol="X" isActive={currentPlayerSymbol === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={currentPlayerSymbol === 'O'} />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
